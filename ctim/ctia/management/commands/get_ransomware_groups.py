7942ba9a9bee9ebdf5363d74fe821ef5ee264232
Matthew Thompson <thompsonson@gmail.com>
2024-04-12 14:05:20 +0200

    chore: logging for ransomware tasks

--- ctim/ctia/management/commands/get_ransomware_groups.py
+++ ctim/ctia/management/commands/get_ransomware_groups.py
@@ -28,6 +28,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         json_file = kwargs["json_file"]
+        self.stdout.write(f"Running handle for json_file: {json_file}")
         try:
             if json_file.startswith("http://") or json_file.startswith("https://"):
                 response = requests.get(json_file)
@@ -38,6 +39,7 @@ class Command(BaseCommand):
                     data = json.load(file)
             for item in data:
                 try:
+                    self.stdout.write(f"Running update_or_create for group: {item['name']}")
                     group, created = Group.objects.update_or_create(
                         name=item["name"],
                         defaults={
